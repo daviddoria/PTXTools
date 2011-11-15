b96bbd4fb451c874bbf6e6fca39e578ee6a9e395
David Doria <daviddoria@gmail.com>
2011-11-15 12:40:54 -0500

    Modified PTXtoRGB.cxx

--- PTXtoRGB.cxx
+++ PTXtoRGB.cxx
@@ -4,16 +4,16 @@ int main (int argc, char *argv[])
 {
   if(argc != 3)
     {
-    std::cout << "Required arguments: InputFilename(ptx) OutputFilename(png)" << std::endl;
+    std::cout << "Required arguments: InputFilename(ptx) OutputPrefix (_RGB.png will automatically be appended)" << std::endl;
     return EXIT_FAILURE;
     }
 
   std::string inputFilename = argv[1];
-  std::string outputFilename = argv[2];
+  std::string outputPrefix = argv[2];
 
   PTXImage ptxImage;
   ptxImage.ReadFile(inputFilename);
-  ptxImage.WriteRGBImage(outputFilename);
+  ptxImage.WriteRGBImage(outputPrefix);
 
   return EXIT_SUCCESS;
 }
